e81b2d3d2d911a258a7fd67abb282e5acb8f7a6a
Peter ALHachem <43495377+PeterALHachem@users.noreply.github.com>
2024-03-17 18:08:00 +0100

    Layout for lab 10

---
+++ ARM/Simon game/Objects/lib_timer.d
@@ -0,0 +1,11 @@
+./objects/lib_timer.o: \
+  ..\..\..\..\Computer\ Architecture\Ferrero\ARM\ samples\15_LPC1768_led_button_power_timer\timer\lib_timer.c \
+  C:\Users\peter_x5l40e2\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.7.1\Device\Include\lpc17xx.h \
+  C:\Users\peter_x5l40e2\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm3.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Users\peter_x5l40e2\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\peter_x5l40e2\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\peter_x5l40e2\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Users\peter_x5l40e2\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\mpu_armv7.h \
+  C:\Users\peter_x5l40e2\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.7.1\Device\Include\system_LPC17xx.h \
+  ..\..\..\..\Computer\ Architecture\Ferrero\ARM\ samples\15_LPC1768_led_button_power_timer\timer\timer.h
